2c68a50d63f9d93e20564e1e1d69a3e883d9d71e
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-16 20:36:42 +0300

    update templates,add routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,12 @@
-from flask import Blueprint
-main = Blueprint('main',__name__)
-from . import views,errors
+from flask import render_template
+from . import main
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Welcome to The best Restaurant '
+    return render_template('checkout.html',title=title)

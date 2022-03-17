2ce5e25d3ed840a77ec9b643a2f24b0b03a0c1ce
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-17 07:52:54 +0300

    Integrated navbar

--- app/main/views.py
+++ app/main/views.py
@@ -2,8 +2,11 @@ from flask import render_template, request, redirect, url_for, abort
 from . import main
 from ..models import User
 from .. import db, photos
+from flask_login import login_required, current_user
+
 
 
 @main.route('/')
 def index():
+    
     return render_template('home.html', name='')
